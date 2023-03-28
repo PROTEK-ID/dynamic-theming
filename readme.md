81ff92dc11788aa4ba7f147a14535140ad7bc901
BanDroid <dbbhuh5@gmail.com>
2023-03-28 21:11:48 +0700

    fix banner and update readme

--- readme.md
+++ readme.md
@@ -1,1 +1,16 @@
+# Dynamic Theme using DaisyUI
+
+using DaisyUI for creating many dynamic themes react app.
+
 Demo: [https://dynamic-theming.pages.dev](https://dynamic-theming.pages.dev)
+
+## Setup
+
+### install dependencies
+
+open the project folder in terminal and
+`npm install`.
+
+### running locally
+
+dont forget to install and run command `npm run dev`, then open the url shown in terminal to your browser.
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,10 @@ export default function Home(): ReactElement {
   return (
     <div className="max-w-md mx-auto p-4">
       <div className="hero min-h-screen">
-        <div className="hero-content flex-col">
+        <div className="hero-content flex-col w-full">
           <img
             src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.oegfCIw7nZgeieBiQhdaWAHaEK%26pid%3DApi&f=1&ipt=85bf69ef05745079e747c167ff7b0c362afdae449cad684718176a653a10999f&ipo=images"
-            className="max-w-sm rounded-lg shadow-2xl"
+            className="max-w-sm w-full rounded-lg shadow-2xl"
           />
           <div>
             <h1 className="text-5xl font-bold">Box Office News!</h1>
